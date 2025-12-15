1f96eeaa56d83cc28f1ea172bcb46770a4229602
Diegoww12a <diegoneves2021123@gmail.com>
2025-12-14 22:26:41 -0300

    Deploy no Netlify

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -19,7 +19,7 @@ export default function LoginPage({ onLogin }: LoginPageProps) {
   setIsLoading(true);
 
   try {
-    const res = await fetch('http://localhost:3000/login', {
+    const res = await fetch('https://francagestao.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ password })
